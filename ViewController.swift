8385c6ca435d423d00fc3d448ee342c575e8e7c4
Namık Karabıyık <namik.karabiyik@ddtech.com.tr>
2022-12-22 19:57:13 +0300

    Initial Commit

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SpeechSynthApp
+//
+//  Created by Namik Karabiyik on 22.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnSpeech: UIButton!
+    @IBOutlet weak var btnChooseLang: UIButton!
+    @IBOutlet weak var txtInput: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+aiföaslmasd
+    şlf aşsd
+
+}
+
