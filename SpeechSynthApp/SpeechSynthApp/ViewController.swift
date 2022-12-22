6d28e20154167c577ee056ef99f028790a6d47fe
Namık Karabıyık <namik.karabiyik@ddtech.com.tr>
2022-12-22 22:29:15 +0300

    initial Commit

--- SpeechSynthApp/SpeechSynthApp/ViewController.swift
+++ SpeechSynthApp/SpeechSynthApp/ViewController.swift
@@ -9,15 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var btnSpeech: UIButton!
-    @IBOutlet weak var btnChooseLang: UIButton!
-    @IBOutlet weak var txtInput: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-aiföaslmasd
-    şlf aşsd
+
 
 }
 
